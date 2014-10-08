6a34cec78344a32c3e7573c4ced6659acbd347e8
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-10-08 21:59:42 +0100

    Add an inital main.c
    This does nothing but configure the clock.
    
    Change-Id: Ife4906d54b417dc32094c954347a57fa2aef9e2c

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/systick.h>
+
+#include "led.h"
+#include "cdcacm.h"
+#include "output.h"
+#include "fan.h"
+#include "smps.h"
+#include "piezo.h"
+#include "button.h"
+#include "battery.h"
+#include "usart.h"
+#include "pswitch.h"
+#include "analogue.h"
+
+void
+init()
+{
+
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPAEN);
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPBEN);
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPDEN);
+}
+
+int
+main()
+{
+
+	init();
+
+	while (1) {
+		// Do things
+	}
+}
