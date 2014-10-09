2505099bddb4bbcdc6b4cd6c590db5a76964955d
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-10-09 01:03:42 +0100

    Extend testscript to send arbitary commands
    I forsee testing the current power board arrangement simply by invoking
    this script to initiate a variety of commands. There'll be a few
    problems with parsing data to send to the board, and printing data once
    it's generated, but we'll cross that bridge when we come to it.
    
    Change-Id: I04f0bd3b1ee1d09b1837d24bd87f883bf6b56993

--- test_frob.py
+++ test_frob.py
@@ -2,7 +2,56 @@
 
 import sys
 import struct
+import argparse
 from usb1 import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument("reqtype", help="read or write")
+parser.add_argument("reqname", help="name of request to make")
+parser.add_argument("argument", help="For writes, an output argument", type=int)
+args = parser.parse_args()
+
+write_ids = {
+        'output1' : 0,
+        'output2' : 1,
+        'output3' : 2,
+        'output4' : 3,
+        'output5' : 4,
+        'output6' : 5,
+        'runled' : 6,
+        'errorled' : 7,
+        }
+
+read_ids = {
+        'output1' : 0,
+        'output2' : 1,
+        'output3' : 2,
+        'output4' : 3,
+        'output5' : 4,
+        'output6' : 5,
+        '5vrail' : 6,
+        'batt' : 7,
+        'button' : 8,
+        'fwver' : 9,
+        }
+
+if args.reqtype == "read":
+    req_map = read_ids
+    is_read = True
+elif args.reqtype == "write":
+    req_map = write_ids
+    is_read = False
+else:
+    print >>sys.stderr, "Unrecognized request type"
+    sys.exit(1)
+
+if args.reqname not in req_map:
+    print >>sys.stderr, "\"{0}\" is not a valid request for {1}".format(args.reqname, args.reqtype)
+    sys.exit(1)
+
+req_id = req_map[args.reqname]
+
+###############################################################################
 
 ctx = USBContext()
 dev = ctx.getByVendorIDAndProductID(0x1bda, 1)
@@ -17,7 +66,9 @@ if handle == None:
     print >>sys.stderr, "Could not open power board"
     sys.exit(1)
 
-handle.controlWrite(0, 64, 0x1234, 0x5678, "")
-ret = handle.controlRead(0x80, 64, 0x1234, 0x5678, 4)
-a, b = struct.unpack("hh", ret)
-print "{:02X}, {:02X}".format(a, b)
+if is_read:
+    ret = handle.controlRead(0x80, 64, req_id, 0, 4)
+    a, b = struct.unpack("hh", ret)
+    print "{:02X}, {:02X}".format(a, b)
+else:
+    handle.controlWrite(0, 64, req_id, args.argument, "")
