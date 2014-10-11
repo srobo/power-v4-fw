bb97d51c513fbc2023701a134df6bf895cd90d07
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-10-11 17:08:33 +0100

    Configure current sense disable pins
    These were previously left unconfigured; they need to be pushed high to
    disable current output from any IC.
    
    Change-Id: Ib696354142c65e8a34666838a18104943c83cb22

--- output.c
+++ output.c
@@ -7,6 +7,9 @@ uint16_t OUTPUT_PIN[6]  = {GPIO10, GPIO11, GPIO6, GPIO7, GPIO8, GPIO9};
 uint32_t OUTPUT_STAT_PORT[6] = {GPIOC,  GPIOC,  GPIOB,  GPIOB,  GPIOB,  GPIOB};
 uint16_t OUTPUT_STAT_PIN[6]  = {GPIO10, GPIO11, GPIO12, GPIO13, GPIO14, GPIO15};
 
+uint32_t OUTPUT_CSDIS_PORT[4] = {GPIOC, GPIOC, GPIOC, GPIOC};
+uint16_t OUTPUT_CSDIS_PIN[4] = {GPIO0, GPIO1, GPIO2, GPIO3};
+
 void output_init(void) {
 	for (int i = 0; i < 6; i++) {
 		gpio_clear(OUTPUT_PORT[i], OUTPUT_PIN[i]);
@@ -16,6 +19,15 @@ void output_init(void) {
 		gpio_clear(OUTPUT_STAT_PORT[i], OUTPUT_STAT_PIN[i]);
 		gpio_set_mode(OUTPUT_STAT_PORT[i], GPIO_MODE_OUTPUT_2_MHZ,
 		              GPIO_CNF_OUTPUT_PUSHPULL, OUTPUT_STAT_PIN[i]);
+	}
+
+	// Configure current-sense disable pins, set initially to high
+	// (disabled)
+	for (int i = 0; i < 4; i++) {
+		gpio_clear(OUTPUT_CSDIS_PORT[i], OUTPUT_CSDIS_PIN[i]);
+		gpio_set_mode(OUTPUT_CSDIS_PORT[i], GPIO_MODE_OUTPUT_2_MHZ,
+		              GPIO_CNF_OUTPUT_PUSHPULL, OUTPUT_CSDIS_PIN[i]);
+		gpio_set(OUTPUT_CSDIS_PORT[i], OUTPUT_CSDIS_PIN[i]);
 	}
 }
 
