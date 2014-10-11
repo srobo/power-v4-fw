2134e1009abc4676ed722c01b70ebd28f00cc4e3
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-10-11 17:26:51 +0100

    Generate a mini state machine for reading currents
    Change-Id: I0bad82d34f506b730cd260c459609214d95666f8

--- output.c
+++ output.c
@@ -10,6 +10,13 @@ uint16_t OUTPUT_STAT_PIN[6]  = {GPIO10, GPIO11, GPIO12, GPIO13, GPIO14, GPIO15};
 uint32_t OUTPUT_CSDIS_PORT[4] = {GPIOC, GPIOC, GPIOC, GPIOC};
 uint16_t OUTPUT_CSDIS_PIN[4] = {GPIO0, GPIO1, GPIO2, GPIO3};
 
+// State of which IC we're reading current sense from
+enum { CSREAD_U5 = 0, CSREAD_U6 = 1, CSREAD_U7 = 2, CSREAD_U8 = 3,
+	CSREAD_MAX = CSREAD_U8} csread_ic = CSREAD_U5;
+
+// Current sense samples for each IC. Indexed by IC, then output.
+uint32_t current_sense_samples[4][2];
+
 void output_init(void) {
 	for (int i = 0; i < 6; i++) {
 		gpio_clear(OUTPUT_PORT[i], OUTPUT_PIN[i]);
@@ -29,6 +36,9 @@ void output_init(void) {
 		              GPIO_CNF_OUTPUT_PUSHPULL, OUTPUT_CSDIS_PIN[i]);
 		gpio_set(OUTPUT_CSDIS_PORT[i], OUTPUT_CSDIS_PIN[i]);
 	}
+
+	// Then enable the first IC.
+	gpio_set(OUTPUT_CSDIS_PORT[0], OUTPUT_CSDIS_PIN[0]);
 }
 
 void output_on(uint8_t n) {
@@ -49,4 +59,15 @@ void output_stat_on(uint8_t n) {
 void output_stat_off(uint8_t n) {
 	if (n > 6) return;
 	gpio_clear(OUTPUT_STAT_PORT[n], OUTPUT_STAT_PIN[n]);
+}
+
+// To be called from ADC intr
+void current_sense_recvsamples(uint32_t samp1, uint32_t samp2)
+{
+
+	current_sense_samples[csread_ic][0] = samp1;
+	current_sense_samples[csread_ic][1] = samp2;
+	csread_ic++;
+	if (csread_ic > CSREAD_MAX)
+		csread_ic = 0;
 }
--- output.h
+++ output.h
@@ -11,3 +11,5 @@ void output_off(uint8_t n);
 void output_stat_on(uint8_t n);
 
 void output_stat_off(uint8_t n);
+
+void current_sense_recvsamples(uint32_t samp1, uint32_t samp2);
