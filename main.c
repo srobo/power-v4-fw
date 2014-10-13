e84fbda28d6e3dbc5dfd0e3a1b87b143170bc3f7
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-10-13 01:15:07 +0100

    Write initial software current limit code
    If we read a sample from the current measurement IC saying that we're
    drawing more than 40A, cut all peripherals, and spin in a loop, beeping.
    
    I can't test a current of 40A in my existing setup, but I'm sure this is
    something we'll end up testing to qualify the firmware.
    
    Change-Id: Ie9b8c24610cdcd85d53e7b1af56d057d1957db02

--- main.c
+++ main.c
@@ -19,6 +19,10 @@
 #include "analogue.h"
 #include "usb.h"
 
+#define delay(x) do { for (int i = 0; i < x * 1000; i++) \
+                          __asm__("nop"); \
+                    } while(0)
+
 void
 init()
 {
@@ -43,6 +47,66 @@ init()
 	analogue_init();
 }
 
+void
+shut_down_everything()
+{
+	unsigned int i;
+	// In case of the battery being low, or drawing too much current,
+	// turn off all our peripherals. This includes turning the fan off,
+	// as that'll eventually knacker the battery by itself.
+	for (i = 0; i < 6; i++)
+		output_off(i);
+
+	for (i = 0; i < 6; i++)
+		output_stat_off(0);
+
+	led_clear(LED_RUN);
+	led_clear(LED_ERROR);
+	led_clear_flat(); // Maybe configure later.
+
+	smps_off();
+
+	fan_off();
+
+	return;
+}
+
+void
+check_batt_undervolt()
+{
+	// EUNIMPLEMENTED
+	return;
+}
+
+void
+check_batt_current_limit()
+{
+	uint32_t current = battery_current();
+	if (current >= 40000) {
+		// Something is wrong.
+		shut_down_everything();
+
+		// At this point, whatever was overloading the battery is now
+		// turned off, or the power board is ablaze. Sit in a loop
+		// beeping, to let the user know something went wrong.
+		//
+		// In this mode, we do not want to knacker the battery either,
+		// so continue to poll for low voltage.
+		while (1) {
+			check_batt_undervolt();
+
+			for (unsigned int i = 0; i < 2000; i++) {
+				piezo_toggle();
+				delay(1);
+			}
+
+			check_batt_undervolt();
+
+			delay(2000);
+		}
+	}
+}
+
 int
 main()
 {
@@ -53,5 +117,7 @@ main()
 		// Do things
 		usb_poll();
 		current_sense_poll();
+		check_batt_current_limit();
+		check_batt_undervolt();
 	}
 }
