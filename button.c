92e0c673ce693a2acb0b878b252ffe838f312f3d
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-10-16 01:03:45 +0100

    Write a force_bootloader function; put in loader
    Via section magic
    
    Change-Id: I989fb7047905625d327b2af66133db1ce92f9977

--- button.c
+++ button.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #include "button.h"
 #include <libopencm3/stm32/gpio.h>
 
@@ -17,6 +19,23 @@ void button_init(void) {
 
 bool button_int_read(void) { return gpio_get(INT_PORT, INT_PIN); }
 bool button_ext_read(void) { return gpio_get(EXT_PORT, EXT_PIN); }
+
+
+bool force_bootloader() __attribute__((section(".bootloader")));
+
+bool force_bootloader()
+{
+	// Function to check whether we should force the bootloader.
+	// Specifically, do that if the start button is pressed on startup
+	gpio_set(INT_PORT, INT_PIN); // Pull-up
+	gpio_set_mode(INT_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, INT_PIN);
+	gpio_set(EXT_PORT, EXT_PIN); // Pull-up
+	gpio_set_mode(EXT_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, EXT_PIN);
+
+	if (!button_int_read() || !button_ext_read())
+		return true;
+	return false;
+}
 
 void button_poll()
 {
