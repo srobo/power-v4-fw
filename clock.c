e35bee19f8a77a5ce2f42791e308b7f1d9550ffe
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-10-18 11:05:43 +0100

    Add a counter/clock timer
    For measuring the passage of time
    
    Change-Id: I2aab91de734234b6d9d8b9bc4fc034c7e072a702

---
+++ clock.c
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/cm3/nvic.h>
+
+#include "clock.h"
+
+static volatile bool clock_fired = false;
+
+void
+clock_init()
+{
+
+	rcc_periph_clock_enable(RCC_TIM3);
+	timer_reset(TIM3);
+	timer_set_prescaler(TIM3, 7200); // 72Mhz -> 10Khz
+	timer_set_period(TIM3, 10); // 10 ticks -> 1Khz
+	nvic_enable_irq(NVIC_TIM3_IRQ);
+	nvic_set_priority(NVIC_TIM3_IRQ, 2); // Less important
+	timer_enable_update_event(TIM3);
+	timer_enable_irq(TIM3, TIM_DIER_UIE);
+	timer_enable_counter(TIM3);
+}
+
+void
+tim3_isr()
+{
+
+	clock_fired = true;
+	return;
+}
---
+++ clock.h
@@ -0,0 +1,2 @@
+// Configure a 1Khz timer for us to make decisions about the passage of time.
+void clock_init(void);
